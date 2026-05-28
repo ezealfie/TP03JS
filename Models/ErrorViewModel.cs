354c0f9c592dcd35610605bad20bd55bceadae63
Android Studio <devandroid@ort>
2026-05-28 07:59:12 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03_ALFIE_FAINSCHTEIN.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
