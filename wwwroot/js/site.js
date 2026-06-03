b5e8fea48c9ffd860dccb4dcdef43e7a69a7b432
Android Studio <devandroid@ort>
2026-06-03 08:41:11 -0300

    terminado faltan espacios

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,14 +4,27 @@
 // Write your JavaScript code.
 
 
-function verificarIntento(){
-let intento = document.getElementById('intento');
-let palabra = document.getElementById('palabra');
-let i = 0;
-let encontrado = false;
-while(i<palabra.length && !encontrado)
-encontrado = palabra[i] === intento
-}
-if(encontrado){
-    document.getElementById('incongitas').innerHTML[i](palabra[i])
+function verificarIntento() {
+    let intento = document.getElementById('intento').value;
+    let palabra = document.getElementById('palabra').value;
+    let resultado = ""; 
+    console.log(palabra)
+    for (i = 0; i < palabra.length; i++) {
+        if (palabra[i].toUpperCase() === intento.toUpperCase()) {
+            resultado += intento.toUpperCase() ;
+        }
+        else {
+            resultado += document.getElementById('incongitas').innerText[i];
+        }
+
+
+    }
+    if (!resultado.includes("_")) {
+        document.getElementById('estado').innerText = 'GANASTE'
+        document.getElementById('estado').style.color = 'green';
+
+    }
+    console.log(resultado);
+    document.getElementById('incongitas').innerText = resultado;
+
 }
